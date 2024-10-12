eaeca622aa8b500ec6168d8e9f8b5a619c91def2
aben-cha <aben-cha@student.1337.ma>
2024-10-12 15:01:56 +0100

    cpp01

--- ex03/HumanB.cpp
+++ ex03/HumanB.cpp
@@ -6,7 +6,7 @@
 /*   By: aben-cha <aben-cha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 14:02:52 by aben-cha          #+#    #+#             */
-/*   Updated: 2024/10/10 14:48:12 by aben-cha         ###   ########.fr       */
+/*   Updated: 2024/10/12 15:01:12 by aben-cha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ HumanB::HumanB(const std::string& Name) : name(Name), weapon(NULL) {
 
 void HumanB::attack() {
     if (!weapon)
-        std::cout << "Wepaon not exist." << std::endl;
+        std::cout << "Weapon not exist." << std::endl;
     else
         std::cout << name << " attacks with their " << weapon->getType() << std::endl;
 }
